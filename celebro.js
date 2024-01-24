12a8b90ed98a3d16282780189d0b6a3ad8722e88
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-01-24 20:05:39 -0300

    desafio Aula 3 /24
    As 20:05

---
+++ celebro.js
@@ -0,0 +1,21 @@
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+// if (currentHour >= 5 && currentHour < 12) {
+//   greetingElement.textContent = "Bom dia";
+// } else if (currentHour >= 12 && currentHour < 18) {
+//   greetingElement.textContent = "Boa tarde";
+// } else {
+//   greetingElement.textContent = "Boa noite";
+// }
+
+// Forma que eu não sabia. 
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+greetingElement.textContent = greetingMessage;
