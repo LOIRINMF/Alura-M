9562d12f8ca0ec36b3deca0603b44ec993c866aa
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2024-01-25 21:49:21 -0300

    json

--- celebro.js
+++ celebro.js
@@ -2,15 +2,15 @@ const greetingElement = document.getElementById("greeting");
 
 const currentHour = new Date().getHours();
 
-// if (currentHour >= 5 && currentHour < 12) {
-//   greetingElement.textContent = "Bom dia";
-// } else if (currentHour >= 12 && currentHour < 18) {
-//   greetingElement.textContent = "Boa tarde";
-// } else {
-//   greetingElement.textContent = "Boa noite";
-// }
+/* if (currentHour >= 5 && currentHour < 12) {
+   greetingElement.textContent = "Bom dia";
+ } else if (currentHour >= 12 && currentHour < 18) {
+   greetingElement.textContent = "Boa tarde";
+ } else {
+   greetingElement.textContent = "Boa noite";
+ }
 
-// Forma que eu não sabia. 
+ Forma que eu não sabia. */
 const greetingMessage =
   currentHour >= 5 && currentHour < 12
     ? "Bom dia"
@@ -19,6 +19,19 @@ const greetingMessage =
       : "Boa noite";
 
 greetingElement.textContent = greetingMessage;
+
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
+  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
+  const numColumns = Math.floor(containerWidth / 195); //número de colunas com base na largura do container
+
+  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(195px, 1fr))`;
+
+});
+
+observer.observe(container);
 
 /**/
 const searchInput = document.getElementById('search-input');
@@ -51,10 +64,7 @@ document.addEventListener('input', function () {
     resultPlaylist.classList.remove('hidden');
     resultArtist.classList.add('hidden');
     return
-  } else {
-    requestApi(searchTerm);
   }
-
-
+  requestApi(searchTerm);
 })
 // CHATGPT criar api em json, js.
